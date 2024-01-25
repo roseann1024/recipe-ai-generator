5b78a325e0cc64f1e58068f115d989cb9f6d2054
Rose Ann Austria <roseann1024@gmail.com>
2024-01-24 18:07:32 -0700

    improved the user experience

--- src/index.js
+++ src/index.js
@@ -13,13 +13,21 @@ function generateRecipe(event) {
   let recipeElement = document.querySelector("#input-box");
   let apiKey = "f78a0dbafabf13190c441b8cod34ftff";
   let context =
-    "You are a genius AI and you will generate a simple and basic recipe according to keyword and provide with HTML basic format for example: <p>Recipe</p>";
+    "You are a genius AI and you will generate a simple and basic recipe according to keyword and provide with HTML basic format for example: <p>Recipe</p> Sign with `SheCodes AI` inside a <strong> element  at the bottom";
   let prompt = `Provide a basic and simple recipe based on ${recipeElement.value} `;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let recipeBox = document.querySelector("#recipe-container");
-  recipeBox.innerHTML = `Generating a recipe of ${recipeElement.value}. Please wait.........`;
+  recipeBox.classList.remove("hidden");
+  recipeBox.innerHTML = `<div class="blink">⏳Generating a recipe of ${recipeElement.value}.......</div>`;
 
+  /*new Typewriter("#recipe-container", {
+    strings: `⏳Generating a recipe of ${recipeElement.value}.......`,
+    autoStart: true,
+    cursor: null,
+    delay: 80,
+  });
+*/
   axios.get(apiUrl).then(showRecipe);
 }
 
