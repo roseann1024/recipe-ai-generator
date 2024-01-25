88d29b9e5bc47aaad10d401ad2e93090854b9840
Rose Ann Austria <roseann1024@gmail.com>
2024-01-24 17:01:12 -0700

    AI API Integration

--- src/index.js
+++ src/index.js
@@ -10,14 +10,18 @@ function showRecipe(response) {
 function generateRecipe(event) {
   event.preventDefault();
 
+  let recipeElement = document.querySelector("#input-box");
   let apiKey = "f78a0dbafabf13190c441b8cod34ftff";
   let context =
     "You are a genius AI and you will generate a simple and basic recipe according to keyword and provide with HTML basic format for example: <p>Recipe</p>";
-  let prompt = "Provide a basic and simple recipe";
+  let prompt = `Provide a basic and simple recipe based on ${recipeElement.value} `;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeBox = document.querySelector("#recipe-container");
+  recipeBox.innerHTML = `Generating a recipe of ${recipeElement.value}. Please wait.........`;
 
   axios.get(apiUrl).then(showRecipe);
 }
 
-let formElement = document.querySelector("#inout-form");
+let formElement = document.querySelector("#input-form");
 formElement.addEventListener("submit", generateRecipe);
